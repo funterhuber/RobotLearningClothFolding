a6aa96364e94f0ab28fe5baaff71a0f0eb3b34c1
Lorenzo Calda <calda.lorenzo09@gmail.com>
2026-05-12 16:42:01 +0200

    script to make dataset relative

---
+++ edit_relative_dataset.py
@@ -0,0 +1,22 @@
+import numpy as np
+from datasets import load_dataset
+from huggingface_hub import login
+from lerobot.datasets.lerobot_dataset import LeRobotDataset
+from lerobot.datasets.dataset_tools import recompute_stats
+
+
+# 2. Load your Hugging Face dataset
+# Because we logged in above, this will successfully bypass the 404 and download from the Hub
+repo_id = "jjr1007/may7_first16_last48_newMethod" 
+print(f"Fetching {repo_id} from the Hub...")
+dataset = LeRobotDataset(repo_id)
+
+# 3. Edit the dataset
+print("Recomputing stats...")
+recompute_stats(dataset, relative_action=True, chunk_size=50, relative_exclude_joints=["gripper"])
+
+# 4. Push it back to the Hub
+print("Pushing updates back to the Hub...")
+dataset.repo_id = "jjr1007/may7_first16_last48_newMethod_relative"
+dataset.push_to_hub()
+print("Done!")
